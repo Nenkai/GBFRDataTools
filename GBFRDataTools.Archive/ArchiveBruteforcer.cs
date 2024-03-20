35f2df8f8756bd066484a0b5872e377af7786856
Nenkai <Nenkai@users.noreply.github.com>
2024-03-20 04:19:54 +0100

    [File List] 94.8% (mainly bruteforce)

--- GBFRDataTools.Archive/ArchiveBruteforcer.cs
+++ GBFRDataTools.Archive/ArchiveBruteforcer.cs
@@ -27,7 +27,13 @@ public class ArchiveBruteforcer
 
     public void Bruteforce()
     {
+        //BruteforceLayoutStagePlacement();
         //BruteforceSounds();
+        //BruteforceFsmQuest();
+        //BruteforceMiscTextures();
+        //BruteforceEffectPrimitive();
+        //BruteforceLipSync();
+        //BruteforceEffectTextureFiles();
 
         /*
         foreach (var t in type)
@@ -40,13 +46,13 @@ public class ArchiveBruteforcer
             BruteforceModel(t);
         */
 
+        foreach (var t in type)
+            BruteforcePrefixListFiles(t);
+
         /*
         BruteforcePrefixWeird("st", "r");
         BruteforcePrefixWeird("ph", "p");
-
-        //BruteforceEffectPrimitive();
-        //BruteforceLipSync();
-        //BruteforceEffectTextureFiles();
+        */
 
         // Slow! at least 2 hours
         /*
@@ -68,11 +74,19 @@ public class ArchiveBruteforcer
         foreach (var file in _archive.ArchiveFilesHashTable.ToList())
         {
             // remove fhd for 4k assets
-            _archive.RegisterFileIfValid(file.Key.Replace("/fhd", ""));
+            _archive.RegisterFileIfValid(file.Key.Replace("ui/fhd", "ui/"));
+            _archive.RegisterFileIfValid(file.Key.Replace("ui/", "ui/fhd"));
+
             _archive.RegisterFileIfValid(file.Key.Replace("eng", "jpn"));
+            _archive.RegisterFileIfValid(file.Key.Replace("jpn", "eng"));
+
             _archive.RegisterFileIfValid(file.Key.Replace("english(us)", "japanese"));
+            _archive.RegisterFileIfValid(file.Key.Replace("japanese", "english(us)"));
+
             _archive.RegisterFileIfValid(file.Key.Replace("/4k", "/2k"));
+
             _archive.RegisterFileIfValid(file.Key.Replace(".wtb", ".tex.texb"));
+
             _archive.RegisterFileIfValid(file.Key.Replace(".tex.texb", ".wtb"));
             _archive.RegisterFileIfValid(file.Key.Replace("seq.bxm", "seq_edit_effect.bxm"));
             _archive.RegisterFileIfValid(file.Key.Replace("seq.bxm", "seq_edit_flags.bxm"));
@@ -86,6 +100,100 @@ public class ArchiveBruteforcer
             _archive.RegisterFileIfValid(file.Key.Replace("seq.bxm", "seq_edit_speed.bxm"));
             _archive.RegisterFileIfValid(file.Key.Replace("seq.bxm", "seq_edit_vib.bxm"));
             _archive.RegisterFileIfValid(file.Key.Replace("seq.bxm", "seq_edit_camera.bxm"));
+        }
+    }
+
+    public void BruteforceFsmQuest()
+    {
+        for (int i = 0; i < 500000; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                _archive.RegisterFileIfValid($"system/fsm/quest/quest_{i:D6}_{j}_fsm_ingame.msg");
+            }
+        }
+    }
+
+    public void BruteforceMiscTextures()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            _archive.RegisterFileIfValid($"ui/layouts/common/image_capture/noatlastextures/cmn_imgcap_location_{i:D2}.tex.texb");
+            _archive.RegisterFileIfValid($"ui/fhd/layouts/common/image_quest/noatlastextures/cmn_imgqst_location_{i:D2}.tex.texb");
+        }
+
+        for (int i = 0; i < 1000; i++)
+        {
+            _archive.RegisterFileIfValid($"ui/fhd/layouts/common/image_item/noatlastextures/cmn_icitm_icon_wbg{i:D3}.wtb");
+        }
+
+        for (int i = 0; i < 100; i++)
+        {
+            for (int j = 0; j < 10000; j++)
+            {
+                _archive.RegisterFileIfValid($"ui/fhd/layouts/common/image_item/noatlastextures/cmn_imgitm_{i:D2}_{j:D4}.wtb");
+                _archive.RegisterFileIfValid($"ui/fhd/layouts/common/image_item/noatlastextures/cmn_imgitm_{i:D2}_{j:D4}_glow.wtb");
+            }
+        }
+
+        for (int i = 0; i < 10000; i++)
+        {
+            _archive.RegisterFileIfValid($"ui/fhd/layouts/common/image_word/noatlastextures/cmn_imgword_location_{i:D4}.wtb");
+            _archive.RegisterFileIfValid($"ui/fhd/layouts/common/image_word/noatlastextures/cmn_imgword_common_{i:D4}.wtb");
+            _archive.RegisterFileIfValid($"ui/fhd/layouts/common/image_word/noatlastextures/cmn_imgword_primalbeasts_{i:D4}.wtb");
+            _archive.RegisterFileIfValid($"ui/fhd/layouts/common/image_word/noatlastextures/cmn_imgword_species_{i:D4}.wtb");
+            _archive.RegisterFileIfValid($"ui/fhd/layouts/common/image_chami/noatlastextures/cmn_imgchm_mob{i:D4}.wtb");
+
+            for (int j = 0; j < 20; j++)
+            {
+                for (int k = 0; k < 5; k++)
+                {
+                    _archive.RegisterFileIfValid($"ui/fhd/layouts/common/image_fate/noatlastextures/cmn_imgfate_{i:D4}_{j:D2}_{k:D2}.wtb");
+
+                }
+            }
+        }
+
+
+        for (int i = 0; i < 0x10000; i++)
+        {
+            foreach (var type in type)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    _archive.RegisterFileIfValid($"ui/layouts/common/chara_voice/noatlastextures/cmn_chrvo_{type}{i:D4}_{j:D2}.tex.texb");
+                    _archive.RegisterFileIfValid($"ui/layouts/common/chara_plprm/noatlastextures/cmn_chrprm_{type}{i:D4}_{j:D2}.tex.texb");
+                    _archive.RegisterFileIfValid($"ui/fhd/layouts/common/image_mainstory/noatlastextures/cmn_imgmain_{type}{i:D4}_{j:D2}.tex.texb");
+                    _archive.RegisterFileIfValid($"ui/layouts/common/image_bookem/noatlastextures/cmn_imgbook_{type}{i:D4}_{j:D2}.wtb");
+                    _archive.RegisterFileIfValid($"ui/fhd/movie/ab_{type}{i:D4}_{j:D2}.bk2");
+                    _archive.RegisterFileIfValid($"ui/layouts/common/image_chrcolor/noatlastextures/cmn_imgcol_{type}{i:D4}_c{j:D2}.wtb");
+
+                }
+
+                _archive.RegisterFileIfValid($"ui/fhd/layouts/telop/bossbattle/noatlastextures/jpn/tlp_bbtl_{type}{i:D4}.tex.texb");
+                _archive.RegisterFileIfValid($"ui/fhd/layouts/pause/limitbonus/noatlastextures/ps_lb_stree_{type}{i:D4}.tex.texb");
+                _archive.RegisterFileIfValid($"ui/fhd/layouts/hud/linklevel/noatlastextures/hud_lnklv_{type}{i:D4}.tex.texb");
+                _archive.RegisterFileIfValid($"ui/fhd/layouts/blacksmith/weapon/noatlastextures/bs_wpn_{type}{i:D4}.tex.texb");
+                _archive.RegisterFileIfValid($"ui/fhd/layouts/common/image_equip/noatlastextures/cmn_imgequ_{type}{i:D4}.wtb");
+                _archive.RegisterFileIfValid($"ui/fhd/layouts/common/image_equip/noatlastextures/cmn_imgequ_s_{type}{i:D4}.wtb");
+
+
+            }
+        }
+    }
+
+    public void BruteforceLayoutStagePlacement()
+    {
+        // Takes a while.
+        for (int j = 0; j < 0x10; j++)
+        {
+            for (int k = 0; k < 100; k++)
+            {
+                for (int i = 0; i < 500000; i++)
+                {
+                    _archive.RegisterFileIfValid($"layout/p{j:X1}{k:D2}/placement_multi_{i:D6}.scene.msg");
+                }
+            }
         }
     }
 
@@ -158,7 +266,6 @@ public class ArchiveBruteforcer
     {
         foreach (var file in Directory.GetFiles(_archive.GetDirectory(), "*.mmat", SearchOption.AllDirectories))
         {
-            /*
             byte[] buf = File.ReadAllBytes(file);
             ModelMaterialSet parse = ModelMaterialSet.Serializer.Parse(buf);
 
@@ -169,7 +276,6 @@ public class ArchiveBruteforcer
                     _archive.RegisterFileIfValid($"texture/4k/{tex.ShaderMapNameHash}.texture");
                 }
             }
-            */
         }
     }
 
@@ -198,7 +304,7 @@ public class ArchiveBruteforcer
         }
     }
 
-    public void BruteforcePrefix(string prefix)
+    public void BruteforcePrefixListFiles(string prefix)
     {
         for (int i = 0; i < 0x10000; i++)
         {
@@ -233,6 +339,8 @@ public class ArchiveBruteforcer
             path = $"model/{prefix}/{prefix}{i:x4}/{prefix}{i:x4}.bxm";
             _archive.RegisterFileIfValid(path);
 
+            path = $"{prefix}/{prefix}{i:x4}/{prefix}{i:x4}.lst";
+            _archive.RegisterFileIfValid(path);
 
             for (int j = 0; j < 10; j++)
             {
