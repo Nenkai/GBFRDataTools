817ba3c5a3f83fef3c13cf378497a2653a353660
Nenkai <Nenkai@users.noreply.github.com>
2024-05-27 16:03:39 +0200

    [UI] Implement ControllerEmParam & other components

---
+++ GBFRDataTools.Files/UI/Components/ItemIconStatus.cs
@@ -0,0 +1,29 @@
+﻿using GBFRDataTools.Hashing;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GBFRDataTools.Files.UI.Components;
+
+// ui::component::ItemIconStatus
+public class ItemIconStatus // : Component
+{
+    public static List<UIPropertyTypeDef> Properties { get; set; } = new()
+    {
+         new UIPropertyTypeDef("Status", FieldType.ObjectRef),
+         new UIPropertyTypeDef("StatusMask", FieldType.ObjectRef),
+         new UIPropertyTypeDef("StatusEffect", FieldType.ObjectRef),
+         new UIPropertyTypeDef("Animator", FieldType.ObjectRef),
+    };
+
+    public static List<UIPropertyTypeDef> GetAllProperties()
+    {
+        var list = new List<UIPropertyTypeDef>();
+        list.AddRange(Component.Properties);
+        list.AddRange(Properties);
+        return list;
+    }
+}
