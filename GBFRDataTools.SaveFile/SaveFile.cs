cbed37896a8fe4aacc4f4777387af60892b36648
Nenkai <Nenkai@users.noreply.github.com>
2026-03-07 17:24:01 +0100

    save file: + documented ids

--- GBFRDataTools.SaveFile/SaveFile.cs
+++ GBFRDataTools.SaveFile/SaveFile.cs
@@ -129,7 +129,7 @@ public class SaveGameFile
         UIntSaveDataUnit? hashSeed = SlotData.UIntTable.FirstOrDefault(e => e.IDType == (uint)UnitType.SAVEDATA_HASHSEED);
         hashSeed ??= new UIntSaveDataUnit()
         {
-            IDType = 1003,
+            IDType = (uint)UnitType.SAVEDATA_HASHSEED,
             ValueData = [(uint)Random.Shared.Next()]
         };
 
@@ -175,6 +175,74 @@ public class SaveGameFile
 
 public enum UnitType
 {
+    // 803/811 - ? (changes on trivial save)
+    // 812 - Game timer (hours played)
+    // 834 - Player Name
+    // 1106 - Commendations?
+    // 1108 - Online Status
+    // 1206 - Health?
+    // 1402 - Weapon Slot ID
+    // 2201 - Current Party (character ids)
+
+    // 2509 - Current Position
+
+    // 4302 - Game Options - Gameplay - Invert Vertical Look
+    // 4303 - Game Options - Gameplay - Invert Horizontal Look
+    // 4305 - Game Options - Gameplay - Camera Repositioning (Bool)
+    // 4307 - Game Options - Gameplay - Battle Camera Repositioning (Bool)
+    // 4310 - Game Options - Gameplay - Auto Dash (Bool)
+    // 4312 - Game Options - Gameplay - Auto Target Switching (Bool)
+    // 4313 - Game Options - Gameplay - Vibration (Bool)
+    // 4315 - Game Options - Gameplay - Difficulty Type
+    // 4316 - Game Options - Gameplay - Assist Mode
+    // 4317 - Game Options - Gameplay - Auto Save (Bool)
+    // 4318 - Game Options - Gameplay - Autoplay Dialogue (Bool)
+    // 4319 - Game Options - Gameplay - Screen Shake (Bool)
+    // 4321 - Game Options - Control Scheme - Guard/Lock On Buttons (Bool)
+    // 4322 - Game Options - Gameplay - Communication Wheel Selection Stick
+    // 4324 - Game Options - Gameplay - Autoplay Fate Episode (Bool)
+    // 4326 - Game Options - Gameplay - Override Hold SBA for Chain Bursting (Bool)
+    // 4327 - Game Options - Gameplay - Skybound Art Target Priority
+    // 4328 - Game Options - Gameplay - Skybound Art Activation
+    // 4329 - Game Options - Gameplay - Camera Terrain Adjustment
+    // 4330 - Game Options - Gameplay - Quest Cutscene Auto-Skip (Bool)
+    // 4331 - Game Options - Gameplay - Loading Screen Skip (Bool)
+    // 4332 - Game Options - Gameplay - Navyrngate (Bool)
+    // 4333 - Game Options - Gameplay - Camera Sensitivity (Vertical)
+    // 4334 - Game Options - Gameplay - Camera Sensitivity (Horizontal)
+    // 4335 - Game Options - Gameplay - Aim Sensitivity (Vertical)
+    // 4336 - Game Options - Gameplay - Aim Sensitivity (Horizontal)
+    // 4337 - Game Options - Gameplay - Camera Smoothing (Bool)
+    // 4338 - Game Options - Gameplay - Battle Camera Correction (Bool)
+    // 4339 - Game Options - Gameplay - Enable Map (Bool)
+    // 4340 - Game Options - Gameplay - Auto Close Map (Bool)
+
+    // 4412 - Game Options - Network Settings - Profile Visibility
+    // 4413 - Co-Op Settings - Edit Profile - Player Name
+    // 4420 - Control Scheme - Scheme Type
+    // 4421 - Game Options - Control Scheme - Skill/Dodge Buttons (Bool)
+
+    // 4501 - Co-Op Settings - Player List - Followed Player Steam IDs
+    // 4601 - Co-Op Settings - Edit Profile - Favorite Character
+    // 4603 - Co-Op Settings - Edit Profile - Badge
+    // 4604 - Co-Op Settings - Edit Profile - About Me
+    // 4605 - Co-Op Settings - Edit Profile - Availability
+    // 4606 - Co-Op Settings - Edit Profile - Availability (again?)
+
+    // 4703 - Player Page - Commendations
+    // 4804 - Most used characters for questing
+    // 4901 - Quests cleared
+
+    // 6201 - Communication - Emotes - Ids (for reordering)
+    // 6301 - Communication - Stickers - Ids (for reordering)
+    // 6401 - Communication - Quick Chat - ?
+    // 6402 - Communication - Quick Chat - Emote
+    // 8908 - Online Status Updates? Incremented when changing online status
+    // 8909 - Online Status Updates 2? Incremented when changing online status
+    // 8916 - Game Options - Keyboard - Battle Controls (Byte Array, Key codes)
+    // 8918 - Game Options - Keyboard - Town Controls (Byte Array, Key codes)
+    // 8934 - Game Options - Keyboard - Photo Mode Controls (Byte Array, Key codes)
+
     /// <summary>
     /// Random value/seed. This is generated at first.
     /// </summary>
